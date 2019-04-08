d71c573a69d8a06d6948b1a3269659c456822503
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 18:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments) {
+  let notEmptyAnymore = [];
+  for (let i = 0; i < musicians.length; i++) {
+  notEmptyAnymore.push(`${musicians[i]} plays ${instruments[i]}`)
+  } return notEmptyAnymore;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i <facts.length) {
+    newFacts.push(facts[i] + '!!!');
+    i++;
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(number) {
+  let newArray = [];
+  do {
+    newArray.push("I love the Beatles!")
+  }
+  while (number < 15);
+  return newArray;
+}
